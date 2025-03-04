25f1ca5e03e6e5daa0423c7bb3429a9e73123c1f
shahabhusain <shahabhussain098123@gmail.com>
2025-03-04 16:17:54 +0500

    Task

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=' text-[44px] font-bold flex item-center justify-center h-screen'>
+     ProductBox Task
+    </div>
+  )
+}
+
+export default App
